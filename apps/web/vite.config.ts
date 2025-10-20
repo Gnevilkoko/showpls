df0c1b143076d27424f2312284b0a422a6009597
Kirill <kirillvershov5500@gmail.com>
2025-10-20 19:47:41 +0300

    fix vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: ".",
+  root: "apps/web",
   build: {
-    outDir: "../../dist/apps/web",
+    outDir: "dist/apps/web",
     emptyOutDir: true,
   },
 })
